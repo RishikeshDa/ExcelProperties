4d58a63f276941c8bd706d7cf9f1ae5ab8eb7613
sagar korde <sagargk1@gmailcom>
2019-10-17 13:48:01 +0530

    If u r too smart then make changes by ur self

--- ExcelProperties/src/com/jbk/Aaaa.java
+++ ExcelProperties/src/com/jbk/Aaaa.java
@@ -2,7 +2,8 @@ package com.jbk;
 
 public class Aaaa {
 	public static void main(String[] args) {
-		System.out.println("hi....");
+		System.out.println("hi......");
+		System.out.println("is it enough");
 	}
 
 }
